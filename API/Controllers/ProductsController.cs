69361d06028ff2382f565843061c70c08ad550e9
NikitaNetajiraoPawar <Nikita96pawar@gmail.com>
2023-03-02 13:20:50 +0530

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string getProducts()
+        {
+            return "This will be list of products";
+
+
+         }
+        [HttpGet("{id}")]
+        public string getProduct(int id)
+        {
+            return "This will be product";
+        }
+    }
+        
+}
